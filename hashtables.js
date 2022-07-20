71bcc7e44f9ff62442c13196d5b1631229daf205
priyaVora13 <vorapriya89@gmail.com>
2022-07-19 21:44:27 -0700

    adding keys collison method

--- hashtables.js
+++ hashtables.js
@@ -1,3 +1,5 @@
+const util = require('util');
+
 class HashTable {
     constructor(size){
       this.data = new Array(size);
@@ -18,7 +20,7 @@ class HashTable {
         console.log(" ");
 
       let address = this._hash(key);
-      console.log("  hash address + " + address);
+      console.log("Hash address + " + address);
       console.log(" ");
       
       if (!this.data[address]) {
@@ -40,10 +42,51 @@ class HashTable {
       }
       return undefined;
     }
+
+    keys() { 
+        let keyset = [];
+        //run loop for every memory space
+        for(let i =0; i < this.data.length; i++) {
+            if(this.data[i]) { 
+                //current key
+                keyset.push(this.data[i][0][0]);
+            } 
+        }
+        console.log(keyset);
+        return keyset;
+    }
+
+    keysWithCollisions() {
+        if (!this.data.length) {
+          return undefined
+        }
+        let result = []
+        // loop through all the elements
+        for (let i = 0; i < this.data.length; i++) {
+            // if it's not an empty memory cell
+            if (this.data[i] && this.data[i].length) {
+              // but also loop through all the potential collisions
+              const l = this.data.length;
+              const currentBucket = this.data[i];
+              if (this.data.length > 1) {
+                for (let j = 0; j < currentBucket.length; j++) {
+                  result.push(currentBucket[j][0])
+                }
+              } else {
+                result.push(currentBucket[0])
+              } 
+            }
+        }
+        console.log(result);
+        return result; 
+      }
+   
   }
   
-  const myHashTable = new HashTable(50);
-  myHashTable.set('grapes', 10000)
-  myHashTable.get('grapes')
+  const myHashTable = new HashTable(2);
+//   myHashTable.set('grapes', 10000)
   myHashTable.set('apples', 9)
-  myHashTable.get('apples')
+  myHashTable.set('oranges', 2);
+  myHashTable.set('pears', 200);
+  myHashTable.keysWithCollisions();
+// myHashTable.keys();
